a894d55e68d4ac0f5cf06e684a46248cdba34c97
Lukas <baltagmatei8@gmail.com>
2026-07-27 12:26:31 +0300

    Refactor MararCropBlock to use ModItems for seeds

--- src/main/java/net/moldovanfoods/mf/block/MararCropBlock.java
+++ src/main/java/net/moldovanfoods/mf/block/MararCropBlock.java
@@ -2,27 +2,22 @@ package net.moldovanfoods.mf.block;
 
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.CropBlock;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.block.state.BlockState;
+import net.moldovanfoods.mf.registry.ModItems;
 
 public class MararCropBlock extends CropBlock {
-    public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 4);
 
     public MararCropBlock(Properties properties) {
         super(properties);
     }
 
     @Override
-    public IntegerProperty getAgeProperty() {
-        return AGE;
-    }
-
-    @Override
-    public int getMaxAge() {
-        return 4;
-    }
-
-    @Override
     protected ItemLike getBaseSeedId() {
-        return net.moldovanfoods.mf.registry.ModItems.MARAR_SEEDS;
+        return ModItems.MARAR_SEEDS;
+    }
+
+    @Override
+    protected int getMaxAge() {
+        return 4;
     }
 }
