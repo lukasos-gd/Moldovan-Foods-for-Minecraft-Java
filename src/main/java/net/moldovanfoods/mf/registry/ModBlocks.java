125d744d797eb40d7371e67032e79c127b1830f8
Lukas <baltagmatei8@gmail.com>
2026-07-27 12:27:45 +0300

    Refactor ModBlocks to use new block registration method

--- src/main/java/net/moldovanfoods/mf/registry/ModBlocks.java
+++ src/main/java/net/moldovanfoods/mf/registry/ModBlocks.java
@@ -1,15 +1,12 @@
 package net.moldovanfoods.mf.registry;
 
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.Identifier;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.minecraft.world.level.material.PushReaction;
 import net.moldovanfoods.mf.MoldovanFoods;
 import net.moldovanfoods.mf.block.MararCropBlock;
 
@@ -17,42 +14,41 @@ public class ModBlocks {
 
     public static final Block MARAR_CROP = register(
             "marar_crop",
-            MararCropBlock::new,
-            BlockBehaviour.Properties.of()
-                    .noCollision()
-                    .randomTicks()
-                    .instabreak()
-                    .sound(SoundType.CROP)
-                    .pushReaction(PushReaction.DESTROY)
+            new MararCropBlock(
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.WHEAT)
+                            .noCollision()
+                            .sound(SoundType.CROP)
+            )
     );
 
-    public static final Block SALT_ORE = register(
-            "salt_ore",
-            Block::new,
-            BlockBehaviour.Properties.of()
-                    .mapColor(MapColor.STONE)
-                    .requiresCorrectToolForDrops()
-                    .strength(3.0f, 3.0f)
-                    .sound(SoundType.STONE)
+    public static final Block SARE_ORE = register(
+            "sare_ore",
+            new Block(
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE)
+            )
     );
 
-    public static final Block DEEPSLATE_SALT_ORE = register(
-            "deepslate_salt_ore",
-            Block::new,
-            BlockBehaviour.Properties.of()
-                    .mapColor(MapColor.DEEPSLATE)
-                    .requiresCorrectToolForDrops()
-                    .strength(4.5f, 4.5f)
-                    .sound(SoundType.DEEPSLATE)
+    public static final Block SARE_DEEPSLATE_ORE = register(
+            "sare_deepslate_ore",
+            new Block(
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_IRON_ORE)
+            )
     );
 
-    private static Block register(String path, java.util.function.Function<BlockBehaviour.Properties, Block> factory, BlockBehaviour.Properties properties) {
-        ResourceKey<Block> key = ResourceKey.create(Registries.BLOCK, Identifier.fromNamespaceAndPath(MoldovanFoods.MOD_ID, path));
-        Block block = factory.apply(properties.setId(key));
-        return Registry.register(BuiltInRegistries.BLOCK, key, block);
+    private static Block register(String path, Block block) {
+        Identifier id = Identifier.fromNamespaceAndPath(
+                MoldovanFoods.MOD_ID,
+                path
+        );
+
+        return Registry.register(
+                BuiltInRegistries.BLOCK,
+                id,
+                block
+        );
     }
 
-    public static void register() {
-        MoldovanFoods.LOGGER.info("Registering blocks for " + MoldovanFoods.MOD_ID);
+    public static void registerModBlocks() {
+        MoldovanFoods.LOGGER.info("Registering Moldovan Foods blocks");
     }
 }
