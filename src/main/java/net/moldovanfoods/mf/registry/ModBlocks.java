09e3eddaa1dbc2daa4eacfa85465a1e22508715b
Lukas <baltagmatei8@gmail.com>
2026-07-27 14:58:37 +0300

    Refactor ModBlocks to use factory methods for block creation

--- src/main/java/net/moldovanfoods/mf/registry/ModBlocks.java
+++ src/main/java/net/moldovanfoods/mf/registry/ModBlocks.java
@@ -2,7 +2,9 @@ package net.moldovanfoods.mf.registry;
 
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
@@ -15,43 +17,37 @@ public class ModBlocks {
 
     public static final Block MARAR_CROP = register(
             "marar_crop",
-            new MararCropBlock(
-                    BlockBehaviour.Properties.ofFullCopy(Blocks.WHEAT)
-                            .noCollision()
-                            .sound(SoundType.CROP)
-            )
+            MararCropBlock::new,
+            BlockBehaviour.Properties.ofFullCopy(Blocks.WHEAT)
+                    .noCollision()
+                    .sound(SoundType.CROP)
     );
 
     public static final Block SALT_ORE = register(
             "salt_ore",
-            new Block(
-                    BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE)
-                            .sound(SoundType.STONE)
-            )
+            Block::new,
+            BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE)
+                    .sound(SoundType.STONE)
     );
 
     public static final Block DEEPSLATE_SALT_ORE = register(
             "deepslate_salt_ore",
-            new Block(
-                    BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_IRON_ORE)
-                            .sound(SoundType.DEEPSLATE)
-            )
+            Block::new,
+            BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_IRON_ORE)
+                    .sound(SoundType.DEEPSLATE)
     );
-
-    private static Block register(String path, Block block) {
-        Identifier id = Identifier.fromNamespaceAndPath(
-                MoldovanFoods.MOD_ID,
-                path
+    private static Block register(String path, java.util.function.Function<BlockBehaviour.Properties, Block> factory, BlockBehaviour.Properties properties) {
+        ResourceKey<Block> key = ResourceKey.create(
+                Registries.BLOCK,
+                Identifier.fromNamespaceAndPath(MoldovanFoods.MOD_ID, path)
         );
 
-        return Registry.register(
-                BuiltInRegistries.BLOCK,
-                id,
-                block
-        );
+        Block block = factory.apply(properties.setId(key));
+
+        return Registry.register(BuiltInRegistries.BLOCK, key, block);
     }
 
     public static void register() {
-        MoldovanFoods.LOGGER.info("Registering Moldovan Foods blocks");
+        MoldovanFoods.LOGGER.info("Registering blocks for " + MoldovanFoods.MOD_ID);
     }
 }
