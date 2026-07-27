7b8649bd2ddcab934a445bdc4ae951f54b732100
Lukas <baltagmatei8@gmail.com>
2026-07-27 10:02:59 +0300

    Implement MararCropBlock with age and seed properties

---
+++ src/main/java/net/moldovanfoods/mf/block/MararCropBlock.java
@@ -0,0 +1,28 @@
+package net.moldovanfoods.mf.block;
+
+import net.minecraft.world.item.ItemLike;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+
+public class MararCropBlock extends CropBlock {
+    public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 4);
+
+    public MararCropBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public IntegerProperty getAgeProperty() {
+        return AGE;
+    }
+
+    @Override
+    public int getMaxAge() {
+        return 4;
+    }
+
+    @Override
+    protected ItemLike getBaseSeedId() {
+        return net.moldovanfoods.mf.registry.ModItems.MARAR_SEEDS;
+    }
+}
