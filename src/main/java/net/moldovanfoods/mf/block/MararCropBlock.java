77616d15b8176daeb76cbba8ef14940f2379001b
Lukas <baltagmatei8@gmail.com>
2026-07-27 14:59:25 +0300

    Add age property and refactor getBaseSeedId method

--- src/main/java/net/moldovanfoods/mf/block/MararCropBlock.java
+++ src/main/java/net/moldovanfoods/mf/block/MararCropBlock.java
@@ -2,21 +2,28 @@ package net.moldovanfoods.mf.block;
 
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.moldovanfoods.mf.registry.ModItems;
 
 public class MararCropBlock extends CropBlock {
+    public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 4);
 
     public MararCropBlock(Properties properties) {
         super(properties);
     }
 
     @Override
-    protected ItemLike getBaseSeedId() {
-        return ModItems.MARAR_SEEDS;
+    public IntegerProperty getAgeProperty() {
+        return AGE;
     }
 
     @Override
     public int getMaxAge() {
         return 4;
+    }
+
+    @Override
+    protected ItemLike getBaseSeedId() {
+        return ModItems.MARAR_SEEDS;
     }
 }
