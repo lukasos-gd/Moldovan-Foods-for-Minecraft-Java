c8924ba10f7baa0d8513c9d5bfcaaf562647263d
Lukas <baltagmatei8@gmail.com>
2026-07-28 13:09:15 +0300

    Update ModBlocks.java

--- src/main/java/net/moldovanfoods/mf/registry/ModBlocks.java
+++ src/main/java/net/moldovanfoods/mf/registry/ModBlocks.java
@@ -6,37 +6,46 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 
 import net.moldovanfoods.mf.MoldovanFoods;
 import net.moldovanfoods.mf.block.MararCropBlock;
 
 public class ModBlocks {
 
+
     public static final Block MARAR_CROP = register(
             "marar_crop",
             MararCropBlock::new,
-            BlockBehaviour.Properties.ofFullCopy(Blocks.WHEAT)
+            BlockBehaviour.Properties.of()
                     .noCollision()
+                    .randomTicks()
+                    .instabreak()
                     .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final Block SALT_ORE = register(
             "salt_ore",
             Block::new,
-            BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE)
+            BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.STONE)
+                    .requiresCorrectToolForDrops()
+                    .strength(3.0f, 3.0f)
                     .sound(SoundType.STONE)
-                    .lootFrom(() -> SALT_ORE)
     );
 
     public static final Block DEEPSLATE_SALT_ORE = register(
             "deepslate_salt_ore",
             Block::new,
-            BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_IRON_ORE)
+            BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.DEEPSLATE)
+                    .requiresCorrectToolForDrops()
+                    .strength(4.5f, 4.5f)
                     .sound(SoundType.DEEPSLATE)
-                    .lootFrom(() -> DEEPSLATE_SALT_ORE)
     );
 
     private static Block register(String path, java.util.function.Function<BlockBehaviour.Properties, Block> factory, BlockBehaviour.Properties properties) {
