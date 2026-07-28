4ba1d822ca09c285793ddd10f2ca65650d7ff057
Lukas <baltagmatei8@gmail.com>
2026-07-28 07:55:07 +0300

    Refactor MararCropBlock by removing age property
    Removed age property and related methods from MararCropBlock.

--- src/main/java/net/moldovanfoods/mf/block/MararCropBlock.java
+++ src/main/java/net/moldovanfoods/mf/block/MararCropBlock.java
@@ -1,28 +1,13 @@
 package net.moldovanfoods.mf.block;
 
 import net.minecraft.world.level.ItemLike;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.moldovanfoods.mf.registry.ModItems;
 
 public class MararCropBlock extends CropBlock {
-    public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 4);
 
     public MararCropBlock(Properties properties) {
         super(properties);
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(AGE);
-    }
-
-    @Override
-    public IntegerProperty getAgeProperty() {
-        return AGE;
     }
 
     @Override
