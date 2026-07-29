1aea31464e68ca68eb795c4cb2312e4a530a52a9
Lukas <baltagmatei8@gmail.com>
2026-07-29 07:47:34 +0300

    Add ModLootTables class for loot table modifications

---
+++ src/main/java/net/lukasosstudios/mf/registry/ModLootTables.java
@@ -0,0 +1,23 @@
+package net.lukasosstudios.mf.registry;
+
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
+import net.lukasosstudios.mf.MoldovanFoods;
+
+public class ModLootTables {
+
+    public static void register() {
+        LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
+            if (source.isBuiltin() && Blocks.SHORT_GRASS.getLootTable().orElse(null) == key) {
+                LootPool.Builder pool = LootPool.lootPool()
+                        .add(LootItem.lootTableItem(ModItems.MARAR_SEEDS))
+                        .when(LootItemRandomChanceCondition.randomChance(0.2f));
+                tableBuilder.withPool(pool);
+            }
+        });
+        MoldovanFoods.LOGGER.info("Registering loot table modifications for " + MoldovanFoods.MOD_ID);
+    }
+}
